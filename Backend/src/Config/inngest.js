cabe4ef2d05ed8f2fed27c347e35eab42e226c76
karan <kve94627@gmail.com>
2025-12-03 01:24:45 +0530

    inngest .js

--- Backend/src/Config/inngest.js
+++ Backend/src/Config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest"
 import { connectDB } from "./db.js"
+import { User } from "../models/user.model.js"
 
 //* Create a client to send and revieve evnets
 export const inngest = new Inngest({ id: "slack-clone" })
