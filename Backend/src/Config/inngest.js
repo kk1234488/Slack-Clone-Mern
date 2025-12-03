a8e411e050ea8b7150cc5d65e4040bfb78f7d526
karan <kve94627@gmail.com>
2025-12-04 04:35:34 +0530

    small delete func issue in inngest.js file

--- Backend/src/Config/inngest.js
+++ Backend/src/Config/inngest.js
@@ -25,7 +25,7 @@ const syncUser = inngest.createFunction(
 
 const deleteUserFromDB = inngest.createFunction(
   {id : "delete-user-from-db"},
-  {event : "clerk.user.deleted"},
+  {event : "clerk/user.deleted"},
   async ({ event }) =>{
     await connectDB()
     const { id } = event.data
